7be2068592489092574ffaf0e34dfe19495639e5
Виталий Грицай <vitaly@ITglobalcomrus.onmicrosoft.com>
2021-02-23 20:44:59 +0300

    tst bot

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import socket
 import telebot
 
 bot = telebot.TeleBot('1296185221:AAEbNrkE3ZMjqGemdp710UOXBwQzThbvKNo')
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+hostname = socket.gethostname()
+
+keyboardNodes = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboardElection = telebot.types.ReplyKeyboardMarkup(True, True)
+
+keyboardNodes.row('TimeDiff','cpu', 'net', 'DePoolBalance', 'WalletBalance', 'TickTock', 'DePoolRequest')
+keyboardElection.row('State','StartEnd')
+
+@bot.message_handler(commands=['node'])
+def send_text(message):
+    bot.send_message(message.chat.id, 'Command', reply_markup=keyboardNodes)
+
+@bot.message_handler(commands=['election'])
+def send_text(message):
+    bot.send_message(message.chat.id, 'Command', reply_markup=keyboardElection)
 
 bot.polling()
